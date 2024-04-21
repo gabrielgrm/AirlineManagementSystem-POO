683219bb95df9830da37b95bece1d604d8ea3b98
Gabriel Rodrigues Magalhães <grm@cesar.school>
2024-04-21 12:08:03 -0300

    fix(tests): teste 1 passou

--- src/br/edu/cesarschool/cc/poo/ac/passagem/Bilhete.java
+++ src/br/edu/cesarschool/cc/poo/ac/passagem/Bilhete.java
@@ -1,6 +1,8 @@
 package br.edu.cesarschool.cc.poo.ac.passagem;
 import br.edu.cesarschool.cc.poo.ac.cliente.Cliente;
 import br.edu.cesarschool.cc.poo.ac.utils.Registro;
+
+import java.sql.SQLOutput;
 import java.time.LocalDateTime;
 
 public class Bilhete extends Registro {
@@ -19,33 +21,33 @@ public class Bilhete extends Registro {
     }
 
     public Cliente getCliente() {
-        return cliente;
+        return this.cliente;
     }
 
     public LocalDateTime getDataHora() {
-        return dataHora;
+        return this.dataHora;
     }
 
     public double getPagamentoEmPontos() {
-        return pagamentoEmPontos;
+        return this.pagamentoEmPontos;
     }
 
     public double getPreco() {
-        return preco;
+        return this.preco;
     }
 
     public Voo getVoo() {
-        return voo;
+        return this.voo;
     }
 
     public double obterValorPago(){
-        return preco - getPagamentoEmPontos();
+        return this.preco - this.getPagamentoEmPontos();
     }
     public double obterValorPontuacao(){
-        return obterValorPago() / 20;
+        return this.obterValorPago() / 20;
     }
 
     public String gerarNumero() {
-        return cliente.getCpf() + voo.getNumeroVoo() + dataHora.getYear() + dataHora.getMonthValue() + dataHora.getDayOfMonth();
+        return this.cliente.getCpf() + this.voo.getNumeroVoo() + this.dataHora.getYear() + this.dataHora.getMonthValue() + this.dataHora.getDayOfMonth();
     }
 }
